8d9ebadcfb06cce79c70c84b1ef3cbe88bd499f9
Yogev Rabl <yrabl@redhat.com>
2016-05-26 11:16:06 +0300

    Added graphs1

---
+++ graphs1.py
@@ -0,0 +1,20 @@
+def graphs(data):
+    for i in data:
+        print('=' * i)
+
+data = [
+    10,
+    3,
+    4,
+    8,
+    12,
+    4,
+    2,
+    0,
+    10,
+    4,
+    3,
+    16,
+]
+
+graphs(data)
