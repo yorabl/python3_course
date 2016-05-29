6b77a03b80c452e9f2ad6094bf19b42d47440c60
Yogev Rabl <yrabl@redhat.com>
2016-05-29 16:15:05 +0300

    fixed it with list comprehension

--- sentencesStatistics.py
+++ sentencesStatistics.py
@@ -9,10 +9,12 @@ print("Result:", result)
 assert result == [8, 2, 7, 6, 5, 5, 2, 3, 6, 6, 4]
 print("OK")
 
+
 def max_word_length(s):
     # ==== YOUR CODE HERE ===
     return max(word_lengths(s))
     # =======================
+
 
 result = max_word_length("Contrary to popular belief Lorem Ipsum is not simply random text")
 print("Result:", result)
