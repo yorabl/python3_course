413fa809924404a0afce8e8e100d0230111c0ce6
Yogev Rabl <yrabl@redhat.com>
2016-05-29 16:12:57 +0300

    fixed funny - learn how to write like this!!!!

--- funny.py
+++ funny.py
@@ -1,10 +1,10 @@
 def funny(s):
     # === YOUR CODE HERE! ===
-    l = s[::-1].split()
-    l2 = []
-    [l2.append(i.capitalize()) for i in l]
-    l2.reverse()
-    return ' '.join(l2)
+#    l = s[::-1].split()
+#    l2 = []
+#    [l2.append(i.capitalize()) for i in l]
+#    l2.reverse()
+    return ' '.join([i[::-1].capitalize() for i in s.split()])
     # =======================
 
 
