868a340f2e0563268353f15ba0406e3b18f74f89
Yogev Rabl <yrabl@redhat.com>
2016-05-29 11:34:03 +0300

    Fixed html generator - it's prettier now :)

--- htmlGenerator.py
+++ htmlGenerator.py
@@ -6,11 +6,13 @@ def ul(l):
     >>> ul(['a','b','c'])
     "<ul>\n\t<li>a</li>\n\t<li>b</li>\n\t<li>c</li>\n</ul>\n"
     """
-    str = '<ul>\n\t<li>{}</li>\n\t<li>'.format(l[0])
-    str += '</li>\n\t<li>'.join(l[1:])
-    str += '</li>\n</ul>\n'
+    tmp_list = []
+    for i in l:
+        tmp_list.append('<li>{}</li>\n'.format(i))
+    result = '''<ul>\n\t{}</ul>\n'''.format('\t'.join(tmp_list))
 
-    return str
+    print('{}'.format(result))
+    return result
 
 
 assert ul(['item 1', 'item 2']) == '<ul>\n\t<li>item 1</li>\n\t<li>item 2</li>\n</ul>\n'
