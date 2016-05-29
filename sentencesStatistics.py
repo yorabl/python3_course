0bc6f2252dfd18475de8abeffd7474346b1a435e
Yogev Rabl <yrabl@redhat.com>
2016-05-29 16:05:27 +0300

    get the number of letters in each word in a given sentence The second function finds the max

---
+++ sentencesStatistics.py
@@ -0,0 +1,22 @@
+def word_lengths(s):
+    # ==== YOUR CODE HERE ===
+    l = []
+    [l.append(len(i)) for i in s.split()]
+    return l
+    # =======================
+
+
+result = word_lengths("Contrary to popular belief Lorem Ipsum is not simply random text")
+print("Result:", result)
+assert result == [8, 2, 7, 6, 5, 5, 2, 3, 6, 6, 4]
+print("OK")
+
+def max_word_length(s):
+    # ==== YOUR CODE HERE ===
+    return max(word_lengths(s))
+    # =======================
+
+result = max_word_length("Contrary to popular belief Lorem Ipsum is not simply random text")
+print("Result:", result)
+assert result == 8
+print("OK")
