c2110d948cc6d46bac9144dd0089dcf198c11de6
Yogev Rabl <yrabl@redhat.com>
2016-06-19 09:42:35 +0300

    doesn't matter, really

--- funny.py
+++ funny.py
@@ -1,9 +1,6 @@
 def funny(s):
     # === YOUR CODE HERE! ===
-#    l = s[::-1].split()
-#    l2 = []
-#    [l2.append(i.capitalize()) for i in l]
-#    l2.reverse()
+
     return ' '.join([i[::-1].capitalize() for i in s.split()])
     # =======================
 
